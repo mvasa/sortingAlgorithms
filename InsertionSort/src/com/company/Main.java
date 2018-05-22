54d26b34713cbc71b2ee176be8b19332227faa09
manoj <manoj.vasa7@gmail.com>
2018-05-21 21:11:02 -0600

    got rid of uncessary whitespace and commented out code

--- InsertionSort/src/com/company/Main.java
+++ InsertionSort/src/com/company/Main.java
@@ -6,14 +6,12 @@ public class Main {
     static String numbers;
     static char [] charArray;
     static int [] numArray;
-    public static void main(String[] args) {
-	//get user input
-    getUserInput();
-    //build array
-    buildArray();
-    //call insertion sort
-    performSort();
 
+    public static void main(String[] args)
+    {
+        getUserInput();
+        buildArray();
+        performSort();
     }
 
     private static void getUserInput()
@@ -21,7 +19,6 @@ public class Main {
         Scanner next = new Scanner(System.in);
         System.out.print("Enter list of numbers to be sorted: ");
         numbers  = next.nextLine();
-      //  System.out.println(numbers);
     }
 
     private static void buildArray()
@@ -48,10 +45,6 @@ public class Main {
                 next++;
             }
         }
-
-      /*  for(int i : numArray){
-            System.out.println(i);
-        }*/
     }
 
     private static void performSort()
@@ -79,7 +72,5 @@ public class Main {
         {
             System.out.print(i + " ");
         }
-
     }
-
 }
